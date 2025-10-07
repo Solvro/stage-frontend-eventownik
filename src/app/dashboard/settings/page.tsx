e3fbe5a70b015a73656128630fc85c3340b338d2
JLizak <jeremiasz.lizak@gmail.com>
2025-10-07 19:52:45 +0200

    deleted code from web-eventownik-v2

--- src/app/dashboard/settings/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function DashboardSettingsPage() {
-  return <h1 className="text-3xl font-bold">Ustawienia konta</h1>;
-}
--- tests/msw/node.ts
+++
@@ -1,5 +0,0 @@
-import { setupServer } from "msw/node";
-
-import { handlers } from "../../src/app/dashboard/events/[id]/participants/table/tests/mocks/handlers.js";
-
-export const server = setupServer(...handlers);
